ea4abc2cbbe1d4227367a8a309488ef31e9ba2e1
thiago <thibastos93@gmail.com>
2019-11-18 15:07:18 -0300

    Adicionando o metodo de adivinhação

--- adivinha.py
+++ adivinha.py
@@ -1,6 +1,33 @@
+import random
 
 print("\n************************************")
 print("* Bem vindo ao jogo de adivinhação *")
 print("************************************\n")
 
-numero_secreto = 50
+numero_secreto = random.randrange(1,100)
+print(numero_secreto)
+
+print("Selecione a dificuldade")
+print("(1) Fácil    (2) Médio (3) Difícil")
+dificuldade = int(input("Escolha: "))
+
+
+for tentativas in range(1, 6):
+
+    chute = int(input("Digite um número: "))
+
+    if chute <= 0 :
+        print("Digite um número positivo!")
+        continue;
+
+    if chute == numero_secreto:
+        print("Você acertou!")
+        break;
+
+    else:
+        if chute > numero_secreto:
+            print("Você chutou um número maior!\n")
+        else:
+            print("Você chutou um número menor!\n")
+
+print("Fim de jogo!")
