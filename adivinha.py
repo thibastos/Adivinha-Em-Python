b6103fa23d63fc743559fb824b457f06d4c74382
Thiago Bastos <thibastos93@gmail.com>
2019-11-18 15:27:27 -0300

    Finalizando jogo!

--- adivinha.py
+++ adivinha.py
@@ -1,48 +1,52 @@
 import random
 
-print("\n************************************")
-print("* Bem vindo ao jogo de adivinhação *")
-print("************************************\n")
+def jogar():
 
-numero_secreto = random.randrange(1,100)
-print(numero_secreto)
+    print("\n************************************")
+    print("* Bem vindo ao jogo de adivinhação *")
+    print("************************************\n")
 
-print("Selecione a dificuldade")
-print("(1) Fácil    (2) Médio (3) Difícil")
-dificuldade = int(input("Escolha: "))
-print("\n")
+    numero_secreto = random.randrange(1,100)
 
-if dificuldade == 1:
-    tentativas = 15
+    print("Selecione a dificuldade")
+    print("(1) Fácil    (2) Médio (3) Difícil")
+    dificuldade = int(input("Escolha: "))
+    print("\n")
 
-if dificuldade == 2:
-    tentativas = 10
+    if dificuldade == 1:
+        tentativas = 15
 
-if dificuldade == 3:
-    tentativas = 5
+    if dificuldade == 2:
+        tentativas = 10
 
-pontos = 1000
+    if dificuldade == 3:
+        tentativas = 5
 
-for chutes in range(tentativas):
+    pontos = 1000
 
-    print("Tentativa {} de {}".format(chutes + 1, tentativas))
-    chute = int(input("Digite um número: "))
+    for chutes in range(tentativas):
 
-    if chute <= 0 :
-        print("Digite um número positivo!")
-        continue;
+        print("Tentativa {} de {}".format(chutes + 1, tentativas))
+        chute = int(input("Digite um número: "))
 
-    pontos = pontos - (abs(numero_secreto - chute))
+        if chute <= 0 :
+            print("Digite um número positivo!")
+            continue;
 
-    if chute == numero_secreto:
-        print("Você acertou!")
-        break;
+        pontos = pontos - (abs(numero_secreto - chute))
 
-    else:
-        if chute > numero_secreto:
-            print("Você chutou um número maior!\n")
+        if chute == numero_secreto:
+            print("Você acertou!")
+            break;
+
         else:
-            print("Você chutou um número menor!\n")
+            if chute > numero_secreto:
+                print("Você chutou um número maior!\n")
+            else:
+                print("Você chutou um número menor!\n")
 
-print("Fim de jogo!")
-print(pontos)
+    print("Fim de jogo!")
+    print(pontos)
+
+if __name__ == '__main__':
+    jogar()
